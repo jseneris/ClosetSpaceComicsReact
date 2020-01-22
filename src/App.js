36d7046e926bc56bb3558baa36bc8aaaefde9c7d
John Seneris <john@covetlyapp.com>
2020-01-22 09:17:41 -0500

    initial authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, {Component} from 'react';
-import logo from './logo125.png';
-import banner from './banner.jpeg';
 import './App.css';
 import Main from './pages/Main'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom'
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   constructor(props){
     super(props);
-}
+  }
 
   render(){
+    const
+    {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
     return (
-      <Main />
+      <div className="App">
+        {user ?
+          <p>Hello, {user.displayName}</p>
+         :
+          <p>Please sign in.</p>}
+
+        {user ? (
+          <div>
+            <button onClick={signOut}>Sign out</button>
+            <Main />
+          </div>
+        )
+          :
+          <button onClick={signInWithGoogle}>Sign in with Google</button>}
+      </div>
     );
   }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+//export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
